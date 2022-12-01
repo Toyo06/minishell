e8cd23fa4026c80e47774172b6dce92aea705cd5
Emin A <116839645+emayia@users.noreply.github.com>
2022-12-01 14:08:20 +0100

    Create check_heredoc function

--- Parsing.c
+++ Parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sroggens <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mayyildi <mayyildi@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 07:56:04 by sroggens          #+#    #+#             */
-/*   Updated: 2022/11/28 11:36:21 by sroggens         ###   ########.fr       */
+/*   Updated: 2022/12/01 14:02:39 by mayyildi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,26 @@ int	errormanagement(t_list **lst)
 		return (0);
 	}
 	return (1);
+}
+
+int	check_heredoc(t_list **lst)
+{
+	t_list	*tmp;
+	char	*str;
+
+	tmp = (*lst);
+	while (tmp)
+	{
+		if (tmp->data == 1)
+		{
+			while (1)
+			{
+				str = readline("> ");
+				if (str && ft_strcmp(str, tmp->next->arg) == 0)
+					break ;
+			}
+		}
+		tmp = tmp->next;
+	}
+	return (0);
 }
