6fadc81949cdac69d6f3585ecf737a51aa3cbda6
Emin A <116839645+emayia@users.noreply.github.com>
2023-03-11 16:34:39 +0100

    Update builtin_export.c: the export function now handles all of the test cases

--- builtins/builtin_export.c
+++ builtins/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: mayyildi <mayyildi@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 19:02:57 by mayyildi          #+#    #+#             */
-/*   Updated: 2023/03/09 13:00:17 by mayyildi         ###   ########.fr       */
+/*   Updated: 2023/03/11 16:33:11 by mayyildi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_update_export(t_env **env, char *name, char *content, int eq_flag)
 	{
 		if (ft_strcmp(name, tmp->name) == 0)
 		{
-			if (eq_flag == 0)
+			if (eq_flag == 1)
 			{
 				free(tmp->content);
 				tmp->content = ft_strdup(content);
@@ -32,27 +32,11 @@ void	ft_update_export(t_env **env, char *name, char *content, int eq_flag)
 		}
 		tmp = tmp->next;
 	}
-	if (eq_flag == 0)
+	if (eq_flag == 1)
 		tmp = create_node_env(ft_strdup(name), ft_strdup(content));
 	else
 		tmp = create_node_env(ft_strdup(name), NULL);
 	add_next_node_env(env, tmp);
-}
-
-int	check_export_arg(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '=')
-			i++;
-		if (!ft_isalnum(str[i]))
-			return (0);
-		i++;
-	}
-	return (1);
 }
 
 void	ft_export(t_list **lst, t_env **env)
@@ -60,27 +44,27 @@ void	ft_export(t_list **lst, t_env **env)
 	t_list	*tmp;
 	char	*equal;
 	int		eq_flag;
+	char	*arg;
 
-	eq_flag = 0;
 	tmp = (*lst);
 	while (tmp->next != NULL && tmp->next->data != 6)
 	{
+		eq_flag = 1;
 		tmp = tmp->next;
-		if (!ft_isalpha(tmp->arg[0]) || !check_export_arg(tmp->arg))
-		{
-			error_msg(5);
-			return ;
-		}
+		if (!ft_isalpha(tmp->arg[0]) || !eq_check(tmp->arg))
+			return (error_msg(5));
 		equal = ft_strchr(tmp->arg, '=');
 		if (equal == NULL)
 		{
-			eq_flag = 1;
+			eq_flag = 0;
 			ft_update_export(env, tmp->arg, NULL, eq_flag);
 			continue ;
 		}
 		*equal = '\0';
-		if (eq_flag == 0)
-			ft_update_export(env, tmp->arg, equal + 1, eq_flag);
+		arg = get_arg(equal);
+		ft_update_export(env, tmp->arg, arg, eq_flag);
+		continue ;
+		free(arg);
 	}
 }
 
@@ -107,7 +91,6 @@ void	ft_printexport(t_env **env)
 		if (tmp->name == NULL)
 		{
 			printf("declare -x ");
-
 			printf("%s\n", tmp->content);
 		}
 		tmp = tmp->next;
