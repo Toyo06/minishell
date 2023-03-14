b2b57b06613a905b29bea0fd22e1c9ee3f27db49
Emin A <116839645+emayia@users.noreply.github.com>
2023-03-14 18:40:35 +0100

    Update error_msg.c

--- utils/error_msg.c
+++ utils/error_msg.c
@@ -6,7 +6,7 @@
 /*   By: mayyildi <mayyildi@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 17:40:03 by mayyildi          #+#    #+#             */
-/*   Updated: 2023/03/08 15:21:38 by mayyildi         ###   ########.fr       */
+/*   Updated: 2023/03/14 18:31:52 by mayyildi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,28 @@ void	error_msg(int i)
 		ft_putstr_fd("minishell: syntax error near unexpected token `newline'\n",
 			2);
 	}
-	if (i == 2)
+	else if (i == 2)
 	{
 		ft_putstr_fd("minishell: syntax error near unexpected token `|'\n",
 			2);
+		g_base.retval.code = 258;
 	}
+}
+
+void	err_msg_exit(int i)
+{
 	if (i == 3)
-	{
-		ft_putstr_fd("minishell: exit: numeric argument required\n",
-			2);
-	}
-	if (i == 4)
+		ft_putstr_fd("minishell: exit: numeric argument required\n", 2);
+	else if (i == 4)
 	{
 		ft_putstr_fd("minishell: exit: too many arguments\n",
 			2);
 		g_base.retval.code = 1;
 	}
+}
+
+void	err_msg_export(int i)
+{
 	if (i == 5)
 	{
 		ft_putstr_fd("minishell: export: not a valid identifier\n",
@@ -44,3 +50,31 @@ void	error_msg(int i)
 	}
 }
 
+void	err_msg_cd(int i)
+{
+	if (i == 6)
+	{
+		ft_putstr_fd("cd: too many arguments\n", 2);
+		g_base.retval.code = 1;
+	}
+	else if (i == 7)
+	{
+		ft_putstr_fd("cd: no home directory\n", 2);
+		g_base.retval.code = 1;
+	}
+	else if (i == 8)
+	{
+		ft_putstr_fd("cd: No such file or directory\n", 2);
+		g_base.retval.code = 1;
+	}
+}
+
+void	err_msg_unset(int i)
+{
+	if (i == 9)
+	{
+		ft_putstr_fd("minishell: unset: not a valid identifier\n",
+			2);
+		g_base.retval.code = 1;
+	}
+}
