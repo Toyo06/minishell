d21387ebcb671bdd9438cc5850fccbbad8ff94f6
Emin A <116839645+emayia@users.noreply.github.com>
2023-03-14 18:39:04 +0100

    Update echo

--- builtins/builtin_echo.c
+++ builtins/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: mayyildi <mayyildi@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 18:45:43 by mayyildi          #+#    #+#             */
-/*   Updated: 2023/03/13 16:44:34 by mayyildi         ###   ########.fr       */
+/*   Updated: 2023/03/14 17:14:57 by mayyildi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,15 @@
 void	ft_echo(t_list	**lst, int fd)
 {
 	t_list	*tmp;
-	int		option;
 
 	tmp = (*lst);
 	tmp = tmp->next;
 	if (tmp == NULL)
 		return (ft_putstr_fd("\n", fd));
-	option = ft_checkoption(tmp->arg);
-	if (option == 1 && tmp->next == NULL)
+	g_base.echo.option = ft_checkoption(tmp->arg);
+	if (g_base.echo.option == 1 && tmp->next == NULL)
 		return ;
-	if (tmp->next != NULL && option == 1)
+	if (tmp->next != NULL && g_base.echo.option == 1)
 		tmp = tmp->next;
 	while (tmp || tmp->next->data != 6)
 	{
@@ -38,7 +37,7 @@ void	ft_echo(t_list	**lst, int fd)
 			ft_putstr_fd(" ", fd);
 		tmp = tmp->next;
 	}
-	if (option == 0)
+	if (g_base.echo.option == 0)
 		ft_putstr_fd("\n", fd);
 	g_base.retval.code = 0;
 }
