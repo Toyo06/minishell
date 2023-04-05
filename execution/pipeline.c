ce967bf7ca7eb365fbd8b48d7a3ed69cd878d85e
sroggens <toyo38110s@gmail.com>
2023-04-05 20:28:03 +0200

    small change for cat | cat | ls

--- execution/pipeline.c
+++ execution/pipeline.c
@@ -6,7 +6,7 @@
 /*   By: sroggens <sroggens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 13:14:27 by mayyildi          #+#    #+#             */
-/*   Updated: 2023/04/05 19:04:04 by sroggens         ###   ########.fr       */
+/*   Updated: 2023/04/05 20:27:12 by sroggens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void	pipeline(t_env **env, t_list **lst)
 	startingpipe(&tmp, env);
 	while (g_base.multipipe.i <= g_base.multipipe.totalpipe)
 	{
-		printthelist(&tmp);
 		actionpipeline(&tmp, env);
 			g_base.multipipe.fdin
 			= g_base.multipipe.pipefd[g_base.multipipe.i][0];
@@ -35,6 +34,11 @@ void	pipeline(t_env **env, t_list **lst)
 		g_base.heredoc.processhere += counthereinpipe(&tmp);
 		g_base.redir.fdcount += countredirinpipe(&tmp);
 		g_base.multipipe.i++;
+		if (g_base.multipipe.i > 1)
+		{
+			close(g_base.multipipe.pipefd[g_base.multipipe.i - 2][1]);
+			close(g_base.multipipe.pipefd[g_base.multipipe.i - 2][0]);
+		}
 	}
 	envofpipeline();
 }
